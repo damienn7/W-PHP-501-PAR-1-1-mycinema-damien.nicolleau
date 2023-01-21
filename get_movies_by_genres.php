b51e4176f5296c4794ea5d35e7af1f434a8dc7e7
damienn7 <damien.nicolleau@epitech.eu>
2023-01-21 14:57:48 +0100

    integration de la recherche par genre, amelioration du rendu visuel

--- get_movies_by_genres.php
+++ get_movies_by_genres.php
@@ -1,10 +1,11 @@
 <?php
 
 // requete a effectuer pour recuperer l'id des films a afficher par genre
+if (isset($_GET["search"])) {
+    $id = $matchGenre[0]["id"];
+    //echo $id;
 
-$id = htmlspecialchars($_GET["id_genre"]);
-
-$stmtQueryGenre = $db->prepare("select id_movie from movie_genre where id_genre=:id");
+    $stmtQueryGenre = $db->prepare("select id_movie from movie_genre where id_genre=:id");
 $stmtQueryGenre->execute([
     "id" => $id,
 ]);
@@ -103,6 +104,113 @@ $end = "
 
 
 $script = $start . $middle . $scriptButtons .$end;
+}else{
+    $id = htmlspecialchars($_GET["id_genre"]);
+
+    $stmtQueryGenre = $db->prepare("select id_movie from movie_genre where id_genre=:id");
+    $stmtQueryGenre->execute([
+        "id" => $id,
+    ]);
+    $title = array();
+    $id_movie = $stmtQueryGenre->fetchAll();
+    //var_dump($id_movie);
+    $count = 0;
+    $buttons = array();
+    foreach ($id_movie as $key => $value) {
+    
+        $stmtQueryGenre = $db->prepare("select title from movie where id=:id");
+        $stmtQueryGenre->execute([
+            "id" => $value["id_movie"],
+        ]);
+    
+        $id_movie = $stmtQueryGenre->fetch();
+        //echo $value["id_movie"];
+        //echo $count;
+        //echo $key;
+        $res = (intval($key) + 1) % 30;
+        //echo " $count";
+        if ($count < 10) {
+            $button = "<button class=\"container_container-main_article-get-movies_by-name_p-link button" . ($count) . "\" id=\"$count\" name=\"visible\" style=\"display:;visibility:;\">" . ($count + 1) . "</button>";
+        }
+    
+        if ($count >= 10) {
+            $button = "<button class=\"container_container-main_article-get-movies_by-name_p-link button" . ($count) . "\" id=\"$count\" name=\"hidden\" style=\"display:none;visibility:hidden;\">" . ($count + 1) . "</button>";
+        }
+    
+        $buttons[$count] = $button;
+        // var_dump($buttons);
+        if ($res == 0) {
+            $count += 1;
+            echo "<p class=\"container_container-main_article-get-movies_by-name_p class$count\" name=\"$count\">" . $id_movie["title"] . "</p>";
+        } else {
+            echo "<p class=\"container_container-main_article-get-movies_by-name_p class$count\" name=\"$count\">" . $id_movie["title"] . "</p>";
+        }
+    
+    }
+    
+    echo "<div class=\"container_container-main_article-get-movies_by-name_p-pagination\">";
+    $array = [];
+    $scriptButtons = "";
+    for ($i = 0; $i < $count + 1; $i++) {
+    
+    
+        echo "$buttons[$i]";
+    
+    
+    
+    
+    
+        if ($i >= 10) {
+            $idButtonToBeHidden = $i - 10;
+            $idButtonToBeVisible = $i;
+            $idButtonEvent = $i - 1;
+            $scriptButtons .= "document.getElementsByClassName(\"button$idButtonEvent\")[0].addEventListener(\"click\",()=>{document.getElementsByClassName(\"button$idButtonToBeHidden\")[0].style.display=\"none\";document.getElementsByClassName(\"button$idButtonToBeHidden\")[0].name = \"hidden\";document.getElementsByClassName(\"button$idButtonToBeHidden\")[0].style.visibility=\"hidden\";document.getElementsByClassName(\"button$idButtonToBeVisible\")[0].style.display=\"\";document.getElementsByClassName(\"button$idButtonToBeVisible\")[0].style.visibility=\"\";document.getElementsByClassName(\"button$idButtonToBeVisible\")[0].name = \"visible\";document.getElementsByName(\"visible\")[0].addEventListener(\"click\",()=>{let id = document.getElementsByName(\"visible\")[0].id;id= parseInt(id);if(id>0){console.log(id);document.getElementsByName(\"visible\")[9].style.display = \"none\";document.getElementsByName(\"visible\")[9].style.visibility = \"hidden\";document.getElementsByName(\"visible\")[9].name = \"hidden\";let newId = id - 1; document.getElementById(newId).style.display = \"\"; document.getElementById(newId).style.visibility = \"\"; document.getElementById(newId).name = \"visible\";}});});\n";
+    
+            //$scriptButtons .= "for(let i = 1;i <$count;i++){let hidden = i + 9;let visible = i - 1; console.log(hidden);console.log(i);document.getElementsByName(\"visible\")[0].addEventListener(\"click\",()=>{document.getElementsByClassName(\"container_container-main_article-get-movies_by-name_p-link\")[hidden].style.display=\"none\";document.getElementsByClassName(\"container_container-main_article-get-movies_by-name_p-link\")[hidden].style.visibility=\"hidden\";document.getElementsByClassName(\"container_container-main_article-get-movies_by-name_p-link\")[visible].style.display=\"\";document.getElementsByClassName(\"container_container-main_article-get-movies_by-name_p-link\")[visible].style.visibility=\"\";});}\n";
+            //$scriptButtons .= "document.getElementsByName(\"visible\")[0].addEventListener(\"click\",()=>{let id = document.getElementsByName(\"visible\")[0].id;id= parseInt(id);if(id>0){console.log(id);document.getElementsByName(\"visible\")[9].style.display = \"none\";document.getElementsByName(\"visible\")[9].style.visibility = \"hidden\";document.getElementsByName(\"visible\")[9].name = \"hidden\";let newId = id - 1; document.getElementById(newId).style.display = \"\"; document.getElementById(newId).style.visibility = \"\"; document.getElementById(newId).name = \"visible\";}});";
+            // $scriptButtons .= "document.getElementsByClassName(\"button$i\")[0].style.display=\"none\";document.getElementsByClassName(\"button$i\")[0].style.visibility=\"hidden\";\n";
+        }
+        // if ($i ==10) {
+        //     echo "<button class=\"container_container-main_article-get-movies_by-name_p-link another\" id=\"$count\">...</button>";
+        // }
+        if ($i != 0) {
+            $middle .= "let els" . $i . " = document.getElementsByClassName(\"class" . $i . "\");Array.from(els" . $i . ").forEach(el => {el.style.display = \"none\"; el.style.visibility=\"hidden\";});\n";
+        }
+        if ($i <= $count) {
+    
+    
+            $display = "";
+            for ($j = 0; $j < $count; $j++) {
+                $display .= "let item$i$j = document.getElementsByClassName(\"class$i\")[$j];item$i$j.style.display = \"\"; item$i$j.style.visibility=\"\";\n";
+            }
+            //ajouter l'affichage du bouton 0
+            $inter = "document.getElementsByClassName(\"button$i\")[0].addEventListener(\"click\",()=>{let allItems = document.getElementsByClassName(\"container_container-main_article-get-movies_by-name_p\");Array.from(allItems).forEach(allEl=>{allEl.style.display = \"none\";allEl.style.visibility=\"hidden\";});" . $display . "});\n";
+            $middle .= "$inter\n";
+            // $middle .= "$scriptButtons\n";
+        }
+    
+    
+    }
+    
+    echo "</div>";
+    
+    $start = "<script async>
+            //window.onload = function(){
+                document.addEventListener('DOMContentLoaded', function () {
+                ";
+    
+    $end = "
+                });
+            //}
+        </script>";
+    
+    
+    $script = $start . $middle . $scriptButtons .$end;
+}
+
+
+
+
 //print_r($script);
 
 ?>
--- index.php
+++ index.php
@@ -12,6 +12,7 @@ include("./config/mysql.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
     <script src="./script/script.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Cinema</title>
 </head>
 
@@ -45,22 +46,33 @@ include("./config/mysql.php");
     <main class="container_container-main">
         <!-- recherche de films par nom, genre, distributeur -->
         <form action="./index.php" class="container_container-main_form-search" method="get">
-            <input type="search" name="search" class="container_container-main_form-search_input-search">
-            <button class="container_container-main_form-search_submit-button" type="submit">
-                send request
+            <!-- <div class="container_container-main_form-search_input-search-div"> -->
+            <input type="search" name="search" class="container_container-main_form-search_input-search"><button type="submit" class="container_container-main_form-search_submit-button"><i class="fa fa-search"></i></button>
+            <!-- </div> -->
+            <button class="container_container-main_form-search_submit-button" type="submit" style="display: none;visibility:hidden;">
+                send request 
             </button>
         </form>
         <article class="container_container-main_article-get-movies">
             <section class="container_container-main_article-get-movies_by-name">
                 <?php if (isset($_GET["id_genre"])): ?>
-
+                    <h1 class="container_container-main_article-get-movies_by-name_h1">Resultats par GENRE</h1>
                     <?php include("./get_movies_by_genres.php"); ?>
 
                 <?php else: ?>
-
+                    <h1 class="container_container-main_article-get-movies_by-name_h1">Resultats par GENRE</h1>
                     <?php include("./get_movies.php"); ?>
 
                 <?php endif; ?>
+            </section>
+            <section class="container_container-main_article-get-movies_by-name none">
+                    <h1 class="container_container-main_article-get-movies_by-name_h1">Resultats par NOM</h1>
+                    <?php foreach ($match as $value): ?>
+
+                        <p class="container_container-main_article-get-movies_by-name_p_title"><?php echo $value["title"];?></p>
+
+                    <?php endforeach; ?>
+
             </section>
         </article>
 
@@ -68,63 +80,75 @@ include("./config/mysql.php");
 
     <script>
         //window.onload = function () {
-            document.getElementsByClassName("container_container-header_nav_ul_li")[1].addEventListener("mouseover", addStyle);
-            function addStyle() {
-                document.getElementsByClassName("container_container-header_nav_ul_li_a")[1].style.color = "gold";
 
-                document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.visibility = "visible";
-                document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.display = "flex";
-                document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.flexDirection = "column";
-                document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.justifyContent = "space-between";
-                // document.body.style.backgroundColor = "black";
+        let elementsp = document.getElementsByClassName("container_container-main_article-get-movies_by-name_p");
 
-                let elements = document.getElementsByClassName("container_container-header_nav_ul_li_ul-request_li");
+        Array.from(elementsp).forEach(element => {
+                element.style.visibility = "visible";
+                element.style.display = "";
+            });
 
-                Array.from(elements).forEach(element => {
-                    element.style.visibility = "visible";
-                    element.style.display = "block";
-                });
+        document.getElementsByClassName("container_container-header_nav_ul_li")[1].addEventListener("mouseover", addStyle);
+        function addStyle() {
+            document.getElementsByClassName("container_container-header_nav_ul_li_a")[1].style.color = "gold";
 
-                let elements2 = document.getElementsByClassName("container_container-header_nav_ul_li_ul-request_li_a");
+            document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.visibility = "visible";
+            document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.display = "flex";
+            document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.flexDirection = "column";
+            document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.justifyContent = "space-between";
+            // document.body.style.backgroundColor = "black";
 
-                Array.from(elements2).forEach(element => {
-                    element.style.visibility = "visible";
-                    element.style.display = "block";
-                });
-            }
+            let elements = document.getElementsByClassName("container_container-header_nav_ul_li_ul-request_li");
+
+            Array.from(elements).forEach(element => {
+                element.style.visibility = "visible";
+                element.style.display = "block";
+            });
+
+            let elements2 = document.getElementsByClassName("container_container-header_nav_ul_li_ul-request_li_a");
+
+            Array.from(elements2).forEach(element => {
+                element.style.visibility = "visible";
+                element.style.display = "block";
+            });
+        }
 
 
 
-            document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].addEventListener("mouseout", removeStyle);
+        document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].addEventListener("mouseout", removeStyle);
 
-            document.getElementsByClassName("container_container-header_nav_ul_li")[1].addEventListener("mouseout", removeStyle);
+        document.getElementsByClassName("container_container-header_nav_ul_li")[1].addEventListener("mouseout", removeStyle);
 
-            function removeStyle() {
-                document.getElementsByClassName("container_container-header_nav_ul_li_a")[1].style.color = "cadetblue";
-                document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.visibility = "hidden";
-                document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.display = "none";
-                document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.flexDirection = "";
-                document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.justifyContent = "";
-                //document.body.style.backgroundColor = "black";
+        function removeStyle() {
+            document.getElementsByClassName("container_container-header_nav_ul_li_a")[1].style.color = "cadetblue";
+            document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.visibility = "hidden";
+            document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.display = "none";
+            document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.flexDirection = "";
+            document.getElementsByClassName("container_container-header_nav_ul_li_ul-request")[0].style.justifyContent = "";
+            //document.body.style.backgroundColor = "black";
 
-                let elements = document.getElementsByClassName("container_container-header_nav_ul_li_ul-request_li");
+            let elements = document.getElementsByClassName("container_container-header_nav_ul_li_ul-request_li");
 
-                Array.from(elements).forEach(element => {
-                    element.style.visibility = "hidden";
-                    element.style.display = "none";
-                });
+            Array.from(elements).forEach(element => {
+                element.style.visibility = "hidden";
+                element.style.display = "none";
+            });
 
-                let elements2 = document.getElementsByClassName("container_container-header_nav_ul_li_ul-request_li_a");
+            let elements2 = document.getElementsByClassName("container_container-header_nav_ul_li_ul-request_li_a");
 
-                Array.from(elements2).forEach(element => {
-                    element.style.visibility = "hidden";
-                    element.style.display = "none";
-                });
-            }
+            Array.from(elements2).forEach(element => {
+                element.style.visibility = "hidden";
+                element.style.display = "none";
+            });
+        }
         //}
     </script>
     <?php if (isset($script)) {
         echo $script;
+    }
+
+    if (isset($scriptName)) {
+        echo $scriptName;
     } ?>
 </body>
 
